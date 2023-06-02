cc1e5d002e967ba7b897d7fb27ac86e51886698a
Lisa Valkevich <lizvalk@mail.ru>
2023-06-03 02:39:28 +0700

    Check build succes3

--- src/test/java/ru/netology/CardOrderTest.java
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -23,7 +23,7 @@ public class CardOrderTest {
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(options);
     }
 
     @AfterEach
